5354f4d0af9212b28a755f89fef6b8a1bfa27ce4
skybosi <skybosi_fxc@163.com>
2015-12-08 21:35:38 +0800

    add a lthread test

---
+++ test/lthread.cpp
@@ -0,0 +1,14 @@
+#include "lthread.h"
+#include <string>
+using namespace std;
+int main(int argc, char **argv)
+{
+	lthread test("../log/log/first.log",4000);
+	int i = 10;
+	while (i--)
+	{
+	  //  cout << "!aaaaaass" <<endl;
+	}
+	test.run();
+	return 0;
+}
